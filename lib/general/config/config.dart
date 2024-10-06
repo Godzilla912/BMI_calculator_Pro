eccc78d9061d4bd3b4267471ece7029e532df6e8
Godzilla912 <dimuthuthushan@gmail.com>
2024-10-06 11:26:49 +0200

    bug fix

--- lib/general/config/config.dart
+++ lib/general/config/config.dart
@@ -3,6 +3,6 @@ class AppConfig{
   static const appName = "BMI CALCULATOR ";
 
   //Start google ads setting
-  static const bannerAdsUnitID = "ca-app-pub-1284553075865321/6210918300";
+  static const bannerAdsUnitID = "ca-app-pub-3940256099942544/6300978111";
 
 }
